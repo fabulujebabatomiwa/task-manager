c0f1056ffa159f8326fb25cc493b12283f786fc6
Fabuluje Babatomiwa <fabulujebabatomiwa@gmail.com>
2026-06-13 19:31:08 +0100

    fix: add Vercel URL to CORS origins

--- task-manager/app/main.py
+++ task-manager/app/main.py
@@ -10,7 +10,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://task-manager-sepia-one-80.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
